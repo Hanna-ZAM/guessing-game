6f5acba651bd6684fa837f5ef25e753f61374d80
Hanna Zalatarevich <a.zoloto@mail.ru>
2022-08-17 00:03:49 +0300

    init: add task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
     }
 
     guess() {
-
+        this.guessNumb=Math.round((this.max+this.min)/2);
+        return this.guessNumb;
     }
 
     lower() {
-
+       this.max=this.guessNumb;
+       return  this.max;
     }
 
     greater() {
-
+        this.min=this.guessNumb;
+        return  this.min;
     }
 }
 
